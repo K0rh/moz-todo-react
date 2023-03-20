f92d60848969b23a4b63ad53b3d70bf42c3f6516
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2023-03-20 19:54:07 +0100

    Updates

---
+++ precache-manifest.2f71c2246b7558773e495218db10484c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2799481f243f538c3ecb",
+    "url": "/todo-react/static/css/main.48a733a1.chunk.css"
+  },
+  {
+    "revision": "2799481f243f538c3ecb",
+    "url": "/todo-react/static/js/main.2799481f.chunk.js"
+  },
+  {
+    "revision": "d46fd180036739e41f59",
+    "url": "/todo-react/static/js/1.d46fd180.chunk.js"
+  },
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "2a36a7a7c6bd989ee2d93193fdb54a3d",
+    "url": "/todo-react/index.html"
+  }
+];
